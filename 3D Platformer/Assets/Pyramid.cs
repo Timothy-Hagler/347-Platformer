451c26b4cf1a66ee95bae4dd69c98b8e014092cd
thagler7545 <tmhglr@gmail.com>
2021-03-26 23:09:27 -0500

    3/26/21 Update
    Installs probuilder pack to the engine so it is easier to model things. The desert island now has much more gameplay. You can unlock the speed ability in game on this island

---
+++ 3D Platformer/Assets/Pyramid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pyramid : MonoBehaviour
+{
+
+    public GameObject beam;
+    public GameObject star;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+
+        if(other.tag.Equals("Player"))
+        {
+            beam.SetActive(true);
+            star.SetActive(true);
+            gameObject.SetActive(false);
+        }
+
+    }
+}
