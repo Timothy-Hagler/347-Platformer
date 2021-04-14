7dc2c9ad22eda18490aff0d2b26d087f78ac5800
thagler7545 <tmhglr@gmail.com>
2021-04-14 18:02:22 -0500

    4/14/21 Update
    Works on the snowballs and they now sorta work. get close to them and press "f" and they will roll in the forward direction of the player. Still working on getting it to be on collision

---
+++ 3D Platformer/Assets/Scripts/ForcePush.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcePush : MonoBehaviour
+{
+    public float pushAmount;
+    public float pushRadius;
+    private PlayerController player;
+
+
+    private void Start()
+    {
+        player = GetComponent<PlayerController>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            DoPush();
+        }
+    }
+
+    private void DoPush()
+    {
+        
+        Collider[] colliders = Physics.OverlapSphere(transform.position, pushRadius);
+
+        foreach(Collider pushObject in colliders)
+        {
+            if(pushObject.CompareTag("Snowball"))
+            {
+                
+                Rigidbody pushedBody = pushObject.GetComponent<Rigidbody>();
+                Debug.Log(pushedBody);
+                pushedBody.AddExplosionForce(pushAmount, Vector3.forward, pushRadius);
+                pushedBody.AddForceAtPosition(player.transform.forward, player.transform.position, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        DoPush();
+       
+    }
+}
